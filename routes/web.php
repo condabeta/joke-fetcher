34eb8536954c2c1d1fc27de8af0b11006db95d0e
condabeta <shirley102bailey@mail.ru>
2026-05-07 04:26:20 +0300

    Schedule the Command

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Joke;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/jokes', function () {
+    return response()->json(Joke::latest()->take(50)->get());
 });
